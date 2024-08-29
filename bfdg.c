2cbf90dcadc7ec76e48f64e2ad07aa0f2336a61e
sj5lee <sj5lee1@gmail.com>
2024-08-29 11:37:10 +0900

    added input overwrite and loop cycle history getting ready for backwards code following did work on arrayimagemovementhandler added consts for modes

--- bfdg.c
+++ bfdg.c
@@ -2,7 +2,7 @@
 // TODO: ask to ignore warning when warnings exist
 // TODO: add features(go backward, scroll tape or code, pointer for code, inject tape or input at any time, set checkpoints, step count, show total step count, auto, auto til condition, better drawcommand)
 // TODO: figure out good command format
-// TODO: fix stuff(line numbers when looping, i/o display with newlines, ending when skipping far ahead, draw after end)
+// TODO: fix stuff(i/o display with newlines)
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -24,11 +24,22 @@ enum
     INPUTENDPOS,
     OUTPOS,
     LINENUM,
-    LINESTARTNUM
+    LINESTARTNUM,
+    INPUTOVERRIDEPOS,
+    LOOPCYCLEPOS
+};
+
+enum
+{
+    EXIT =-1,
+    STOP,
+    FORWARD,
+    BACKWARD,
+    AUTO
 };
 
 int isbfcode(char);
-void arrayimagemovementhandler(int *, int,  int *, int);
+void arrayimagemovementhandler(int *, int, int, int);
 void drawcode(int *, char *);
 void drawtape(int *,unsigned char *);
 void drawin(int *, char *);
@@ -44,6 +55,8 @@ int main()
     char bfcode[20000];
     char input[1000] = {0};
     char output[1000] = {0};
+    unsigned char inputoverride[1000]={0};
+    int loopcyclerecord[10000]={0};
     char *inputend = input;
     char *codep = bfcode;
     char *codepend = bfcode;
@@ -96,7 +109,7 @@ int main()
         return 0;
     }
 
-    paramarr[MODE] = 0;
+    paramarr[MODE] = FORWARD;
     paramarr[CODEPOS] = 0;
     paramarr[CODEENDPOS] = codepend - bfcode;
     paramarr[TAPEPOS] = 0;
@@ -106,6 +119,8 @@ int main()
     paramarr[OUTPOS] = 0;
     paramarr[LINENUM] = 1;
     paramarr[LINESTARTNUM] = 0;
+    paramarr[INPUTOVERRIDEPOS] = 0;
+    paramarr[LOOPCYCLEPOS] = 0;
 
     codep = bfcode;
     while (!isbfcode(*codep))
@@ -119,10 +134,10 @@ int main()
     paramarr[CODEPOS] = codep - bfcode;
     if (codep == codepend)
     {
-        paramarr[MODE] = -1;
+        paramarr[MODE] = EXIT;
     }
 
-    while (paramarr[MODE] != -1)
+    while (paramarr[MODE] != EXIT)
     {
         // draw
         drawcode(paramarr, bfcode);
@@ -200,8 +215,20 @@ void drawcode(int *param, char *code)
     }
 }
 
-void arrayimagemovementhandler(int *arrstart, int arrl, int *ppos, int move)
+void arrayimagemovementhandler(int *arrstart, int arrl, int ppos, int move)
 {
+    int buffer=1;
+    ppos=ppos+move;
+
+    if(*ppos<0+buffer){
+        *arrstart=0;
+    }
+    else if(*ppos<*arrstart+buffer){
+        *arrstart=*ppos-buffer;
+    }
+    else if(*ppos>*arrstart+arrl-buffer){
+        *arrstart=*ppos+buffer;
+    }
 }
 
 void drawtape(int *param, unsigned char *tape)
@@ -299,6 +326,7 @@ void drawcommand()
 
 commandstruct takecommand()
 {
+    //current command: {number}{f|b|a|e}
     char c;
     char commandstr[100];
     char *commandp=commandstr;
@@ -306,6 +334,11 @@ commandstruct takecommand()
     int scancheck;
 
     c=getc(stdin);
+    if(c=='-'||c=='+'){
+        *commandp=c;
+        commandp=commandp+1;
+        c=getc(stdin);
+    }
     while('0'<=c&&c<='9'){
         *commandp=c;
         commandp=commandp+1;
@@ -316,6 +349,7 @@ commandstruct takecommand()
         *commandp=c;
         commandp=commandp+1;
         if(c=='\n'){
+            *commandp=0;
             break;
         }
         c=getc(stdin);
@@ -335,10 +369,34 @@ commandstruct takecommand()
 
 void docommand(commandstruct command, int *param, char *code, char *tape, char *input, char *output)
 {
+    //MODE -1 exit, 0 stop, 1 forward, 2 backward, 3 auto 
     int loopstack = 0;
     int i;
+    int exitflag=0;
+    char *chp;
 
-    if (command.direction != 0)
+    chp=command.other;
+    while(*chp!=0){
+        if(*chp=='f'||*chp=='F'){
+            param[MODE]=FORWARD;
+            break;
+        }
+        if(*chp=='b'||*chp=='B'){
+            param[MODE]=BACKWARD;
+            break;
+        }
+        if(*chp=='a'||*chp=='A'){
+            param[MODE]=AUTO;
+            break;
+        }
+        if(*chp=='e'||*chp=='E'||*chp=='q'||*chp=='Q'){
+            exitflag=1;
+            break;
+        }
+        chp=chp+1;
+    }
+
+    if ((param[MODE]==FORWARD&&command.direction>0)||(param[MODE]==BACKWARD&&command.direction<0)||(param[MODE]==AUTO&&command.direction>0))
     {
         for (i = 0; i < command.steps; i++)
         {
@@ -397,6 +455,11 @@ void docommand(commandstruct command, int *param, char *code, char *tape, char *
                         {
                             loopstack--;
                         }
+                        if (code[param[CODEPOS]-1] == '\n')
+                        {
+                            param[LINENUM]++;
+                            param[LINESTARTNUM] = param[CODEPOS];
+                        }
                     }
                 }
                 break;
@@ -416,6 +479,15 @@ void docommand(commandstruct command, int *param, char *code, char *tape, char *
                         {
                             loopstack--;
                         }
+                        if (code[param[CODEPOS]] == '\n')
+                        {
+                            param[LINENUM]--;
+                            param[LINESTARTNUM] = param[CODEPOS];
+                            while(code[param[LINESTARTNUM-1]!='\n']&&param[LINESTARTNUM]!=0)
+                            {
+                                param[LINESTARTNUM]--;
+                            }
+                        }
                     }
                 }
                 break;
@@ -429,7 +501,7 @@ void docommand(commandstruct command, int *param, char *code, char *tape, char *
                 param[CODEPOS]++;
                 if (param[CODEPOS] == param[CODEENDPOS])
                 {
-                    param[MODE] = -1;
+                    param[MODE] = EXIT;
                     break;
                 }
                 if (code[param[CODEPOS] - 1] == '\n')
@@ -438,9 +510,135 @@ void docommand(commandstruct command, int *param, char *code, char *tape, char *
                     param[LINESTARTNUM] = param[CODEPOS];
                 }
             } while (!isbfcode(code[param[CODEPOS]]));
-            if (param[MODE]==-1){
+            if (param[MODE]==EXIT)
+            {
                 break;
             }
         }
+    }
+    if ((param[MODE]==FORWARD&&command.direction<0)||(param[MODE]==BACKWARD&&command.direction>0)||(param[MODE]==AUTO&&command.direction<0))
+    {
+        for (i = 0; i < command.steps; i++)
+        {
+            loopstack=0;
+
+            do
+            {
+                param[CODEPOS]--;
+                if (param[CODEPOS] == 0)
+                {
+                    //TODO:change to just stop
+                    param[MODE] = EXIT;
+                    break;
+                }
+                if (code[param[CODEPOS]] == '\n')
+                {
+                    param[LINENUM]--;
+                    param[LINESTARTNUM] = param[CODEPOS];
+                    while(code[param[LINESTARTNUM-1]!='\n']&&param[LINESTARTNUM]!=0)
+                    {
+                        param[LINESTARTNUM]--;
+                    }
+                }
+            } while (!isbfcode(code[param[CODEPOS]]));
+
+            switch (code[param[CODEPOS]])
+            {
+            case '+':
+                tape[param[TAPEPOS]]++;
+                break;
+
+            case '-':
+                tape[param[TAPEPOS]]--;
+                break;
+
+            case '>':
+                if (param[TAPEPOS] > 0)
+                {
+                    param[TAPEPOS]--;
+                }
+                break;
+
+            case '<':
+                param[TAPEPOS]++;
+                break;
+
+            case '.':
+                output[param[OUTPOS]] = 0;
+                param[OUTPOS]--;
+                break;
+
+            case ',':
+                //TODO: choose between forgeting or keeping past inputs
+                //TODO: save overwritten spaces
+                param[INPUTPOS]--;
+                break;
+
+            case '[':
+                //TODO: add loop cycle tracker
+                if (tape[param[TAPEPOS]] == 0)
+                {
+                    loopstack = 1;
+                    while (loopstack != 0)
+                    {
+                        param[CODEPOS]++;
+                        if (code[param[CODEPOS]] == '[')
+                        {
+                            loopstack++;
+                        }
+                        if (code[param[CODEPOS]] == ']')
+                        {
+                            loopstack--;
+                        }
+                        if (code[param[CODEPOS]-1] == '\n')
+                        {
+                            param[LINENUM]++;
+                            param[LINESTARTNUM] = param[CODEPOS];
+                        }
+                    }
+                }
+                break;
+
+            case ']':
+                if (tape[param[TAPEPOS]] != 0)
+                {
+                    loopstack = -1;
+                    while (loopstack != 0)
+                    {
+                        param[CODEPOS]--;
+                        if (code[param[CODEPOS]] == '[')
+                        {
+                            loopstack++;
+                        }
+                        if (code[param[CODEPOS]] == ']')
+                        {
+                            loopstack--;
+                        }
+                        if (code[param[CODEPOS]] == '\n')
+                        {
+                            param[LINENUM]--;
+                            param[LINESTARTNUM] = param[CODEPOS];
+                            while(code[param[LINESTARTNUM-1]!='\n']&&param[LINESTARTNUM]!=0)
+                            {
+                                param[LINESTARTNUM]--;
+                            }
+                        }
+                    }
+                }
+                break;
+
+            default:
+                break;
+            }
+
+            if (param[MODE]==EXIT)
+            {
+                break;
+            }
+        }
+    }
+    
+    if(exitflag>0){
+        param[MODE]=-1;
     }
 }
